94d1874ee01d0820a5b563723288c952c88e423a
Zhereng Zhang <zherengz@andrew.cmu.edu>
2016-04-12 21:13:06 -0400

    test file

---
+++ serial/src/test.cpp
@@ -0,0 +1,14 @@
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+#include <iostream>
+
+
+int main()
+{
+  cv::Mat image;
+  std::cout << "this worked lol" << std::endl;
+
+  return 0;
+}
