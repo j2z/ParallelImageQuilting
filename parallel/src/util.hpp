0a5c750d088546e069c204e00971a88849446dbb
Randy L Zhao <rlzhao@ghc42.ghc.andrew.cmu.edu>
2016-05-06 22:15:39 -0400

    changed constants util to not have constants

--- parallel/src/util.hpp
+++ parallel/src/util.hpp
@@ -2,14 +2,6 @@
 #ifndef UTIL_H
 #define UTIL_H
 
-#define TILE_HEIGHT 80
-#define TILE_WIDTH 80
-#define CIRCLE_RADIUS 30
-#define WIDTH_TILES 12
-#define HEIGHT_TILES 8
-#define NUM_ITERATIONS 20
-
-
 inline unsigned char imgGet(unsigned char* img, int width, int row, int col, int channel)
 {
   return *(img + (row*width + col)*3 + channel);
