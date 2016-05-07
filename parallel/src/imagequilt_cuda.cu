efe82968cd980d1936a51527377384e1ed859df8
Randy L Zhao <rlzhao@ghc41.ghc.andrew.cmu.edu>
2016-05-07 18:23:02 -0400

    Added timing to main. Added parallel RNG

--- parallel/src/imagequilt_cuda.cu
+++ parallel/src/imagequilt_cuda.cu
@@ -6,18 +6,29 @@
 #include <cuda.h>
 #include <cuda_runtime.h>
 #include <driver_functions.h>
-
+#include <curand.h>
+#include <curand_kernel.h>
 #define NUM_ITERATIONS 20
+#define BLOCK_SIZE 32
 
-void imagequilt_cuda(int texture_width, int texture_height, unsigned char* source, unsigned char* output)
+__global__ void initRandom(unsigned int seed, curandState* states)
+{
+  int idX = blockIdx.x * BLOCK_SIZE + threadIdx.x;
+  curand_init(seed, idX,0,&states[idX]);
+}
+
+__global__ void kernelRandomOutput(curandState* states, int* output, int output_width, int source_size)
+{
+  int idX = blockIdx.x * BLOCK_SIZE + threadIdx.x;
+  output[idX] = curand(&states[idX]) % source_size;
+
+}
+
+void imagequilt_cuda(int texture_width, int texture_height, unsigned char* source, int* output)
 {
   //initialize CUDA global memory
   unsigned char* source_cuda;
-  unsigned char* output_cuda;
-  float* errors_cuda; //cost of the old seams
-  float* energy_cuda; //differences between the source patch and output (maybe not necessary and just use polar)
-  float* energy_polar;
-  float* min_seam; //smallest acculumation of energy from that pixel
+  int* output_cuda;
   //actually, I think it might be possible to store the previous 2 values in shared memory
   unsigned char* back_pointers;
 
@@ -27,21 +38,32 @@ void imagequilt_cuda(int texture_width, int texture_height, unsigned char* sourc
   int num_tiles = HEIGHT_TILES*WIDTH_TILES;
 
   size_t source_size = sizeof(unsigned char)*texture_width*texture_height*3;
-  size_t output_size = sizeof(unsigned char)*output_width*output_height*3;
-  size_t errors_size = sizeof(float)*output_width*output_height;
-  size_t float_polar_size = sizeof(float)*tile_size*num_tiles;
+  size_t output_size = sizeof(int)*output_width*output_height;
+
+  //size_t errors_size = sizeof(float)*output_width*output_height;
+  //size_t float_polar_size = sizeof(float)*tile_size*num_tiles;
   size_t char_polar_size = sizeof(unsigned char)*tile_size*num_tiles;
 
   cudaMalloc((void**)&source_cuda, source_size);
   cudaMalloc((void**)&output_cuda, output_size);
-  cudaMalloc((void**)&errors_cuda, errors_size);
-  cudaMalloc((void**)&energy_cuda, errors_size);
-  cudaMalloc((void**)&energy_polar, float_polar_size);
-  cudaMalloc((void**)&min_seam, float_polar_size);
   cudaMalloc((void**)&back_pointers, char_polar_size);
 
   cudaMemcpy(source_cuda, source, source_size, cudaMemcpyHostToDevice);
-  cudaMemcpy(output_cuda, output, output_size, cudaMemcpyHostToDevice);
+
+  //first copy random pixels from source to output
+  int seed = 15418;
+  curandState *randStates;
+  cudaMalloc((void**)&randStates, sizeof(curandState)*output_width*output_height);
+  int numBlocks = output_width*output_height/BLOCK_SIZE;
+
+  initRandom<<<numBlocks, BLOCK_SIZE>>>(seed, randStates);
+  kernelRandomOutput<<<numBlocks, BLOCK_SIZE>>>(randStates, 
+                                                output_cuda,
+                                                output_width,
+                                                texture_width*texture_height);
+
+  dim3 blockDim(32, 1);
+  dim3 gridDim(num_tiles);
 
   for (int iter = 0; iter < NUM_ITERATIONS; iter++)
   {
@@ -49,17 +71,24 @@ void imagequilt_cuda(int texture_width, int texture_height, unsigned char* sourc
     const int randX = std::rand() % (TILE_WIDTH);
     const int randY = std::rand() % (TILE_HEIGHT);
 
-    //choose random patch from source and calculates the energy of each patch
-
+    //copy random pixels over
+    //choose random patch from source
     //convert energies in polar and perform DP
-
+    //copy the pixels over
     //check whether there was an improvement???
 
+    
+    //dim3 blockDim(32, 1);
+    //dim3 gridDim(num_tiles/8);
+
+    
   }
 
+  cudaMemcpy(output, output_cuda, output_size, cudaMemcpyDeviceToHost);
 
-
+  cudaFree(randStates);
   cudaFree(source_cuda);
   cudaFree(output_cuda);
-  cudaFree(errors_cuda);
 }
+
+
