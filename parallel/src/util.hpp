b441948063a83243b7160ed38795af5faa43f84f
Randy L Zhao <rlzhao@ghc41.ghc.andrew.cmu.edu>
2016-05-07 15:41:56 -0400

    Added getRefIndex imGetRef and imgSetRef(last 2 are for output only)

--- parallel/src/util.hpp
+++ parallel/src/util.hpp
@@ -2,6 +2,21 @@
 #ifndef UTIL_H
 #define UTIL_H
 
+inline int getRefIndx(int width, int row, int col)
+{
+  return row*width + col;
+}
+
+inline int imgGetRef(int* img, int width, int row, int col)
+{
+  return *(img + row*width + col);
+}
+
+inline void imgSetRef(int* img, int width, int row, int col, int val)
+{
+  *(img + row*width + col) = val;
+}
+
 inline unsigned char imgGet(unsigned char* img, int width, int row, int col, int channel)
 {
   return *(img + (row*width + col)*3 + channel);
