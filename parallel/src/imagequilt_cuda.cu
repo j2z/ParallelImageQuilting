18089cce0b528e84aa84f2dd9a0e71f4a33204cb
Randy L Zhao <randylzhao@gmail.com>
2016-05-08 19:04:07 -0400

    Fixed an annoying bug

--- parallel/src/imagequilt_cuda.cu
+++ parallel/src/imagequilt_cuda.cu
@@ -8,7 +8,7 @@
 #include <curand.h>
 #include <curand_kernel.h>
 #define NUM_ITERATIONS 20
-#define BLOCK_SIZE 32
+#define BLOCK_SIZE 64
 
 #define LOG2_WARP_SIZE 5U
 #define WARP_SIZE (1U << LOG2_WARP_SIZE)
@@ -17,7 +17,7 @@
 //Almost the same as naive scan1Inclusive, but doesn't need __syncthreads()
 //assuming size <= WARP_SIZE
 inline __device__ uint
-warpScanInclusive(int threadIndex, uint idata, volatile uint *s_Data, uint size){
+warpScanInclusive(int threadIndex, float idata, volatile float *s_Data, uint size){
   uint pos = 2 * threadIndex - (threadIndex & (size - 1));
   s_Data[pos] = 0;
   pos += size;
@@ -29,19 +29,19 @@ warpScanInclusive(int threadIndex, uint idata, volatile uint *s_Data, uint size)
   return s_Data[pos];
 }
 
-inline __device__ uint warpScanExclusive(int threadIndex, uint idata, volatile uint *sScratch, uint size){
+inline __device__ float warpScanExclusive(int threadIndex, float idata, volatile float *sScratch, uint size){
   return warpScanInclusive(threadIndex, idata, sScratch, size) - idata;
 }
 
 __inline__ __device__ void
-sharedMemExclusiveScan(int threadIndex, uint* sInput, uint* sOutput, volatile uint* sScratch, uint size)
+sharedMemExclusiveScan(int threadIndex, float* sInput, float* sOutput, volatile float* sScratch, uint size)
 {
   if (size > WARP_SIZE) {
 
-    uint idata = sInput[threadIndex];
+    float idata = sInput[threadIndex];
 
     //Bottom-level inclusive warp scan
-    uint warpResult = warpScanInclusive(threadIndex, idata, sScratch, WARP_SIZE);
+    float warpResult = warpScanInclusive(threadIndex, idata, sScratch, WARP_SIZE);
 
     // Save top elements of each warp for exclusive warp scan sync
     // to wait for warp scans to complete (because s_Data is being
@@ -56,7 +56,7 @@ sharedMemExclusiveScan(int threadIndex, uint* sInput, uint* sOutput, volatile ui
 
     if ( threadIndex < (SCAN_BLOCK_DIM / WARP_SIZE)) {
       // grab top warp elements
-      uint val = sScratch[threadIndex];
+      float val = sScratch[threadIndex];
       // calculate exclusive scan and write back to shared memory
       sScratch[threadIndex] = warpScanExclusive(threadIndex, val, sScratch, size >> LOG2_WARP_SIZE);
     }
@@ -67,20 +67,20 @@ sharedMemExclusiveScan(int threadIndex, uint* sInput, uint* sOutput, volatile ui
     sOutput[threadIndex] = warpResult + sScratch[threadIndex >> LOG2_WARP_SIZE] - idata;
 
     } else if (threadIndex < WARP_SIZE) {
-      uint idata = sInput[threadIndex];
+      float idata = sInput[threadIndex];
       sOutput[threadIndex] = warpScanExclusive(threadIndex, idata, sScratch, size);
     }
 }
 
 __global__ void initRandom(unsigned int seed, curandState* states)
 {
-  int idX = blockIdx.x * BLOCK_SIZE + threadIdx.x;
+  int idX = (blockIdx.y*BLOCK_SIZE + blockIdx.x) * BLOCK_SIZE + threadIdx.x;
   curand_init(seed, idX,0,&states[idX]);
 }
 
 __global__ void kernelRandomOutput(curandState* states, int* output, int output_width, int source_size)
 {
-  int idX = blockIdx.x * BLOCK_SIZE + threadIdx.x;
+  int idX = (blockIdx.y*BLOCK_SIZE + blockIdx.x) * BLOCK_SIZE + threadIdx.x;
   output[idX] = curand(&states[idX]) % source_size;
 }
 
@@ -97,6 +97,9 @@ __global__ void kernelFindBoundaries(curandState* states, unsigned char* source,
   __shared__ float array2[POLAR_WIDTH];
   __shared__ char back_pointers[POLAR_HEIGHT][POLAR_WIDTH];
 
+  __shared__ float scratch[POLAR_WIDTH];
+  __shared__ float scanOut[POLAR_WIDTH];
+
   if (colIdx == 0)
   {
     samplesX[tileIdx] = curand(&states[idX]) % (sourceWidth - 2*MAX_RADIUS) + MAX_RADIUS;
@@ -109,6 +112,12 @@ __global__ void kernelFindBoundaries(curandState* states, unsigned char* source,
 
   int mapX = tileX * TILE_WIDTH + TILE_WIDTH / 2 + xOffset;
   int mapY = tileY * TILE_HEIGHT + TILE_HEIGHT / 2 + yOffset;
+
+  float* current_array = array1;
+  current_array[colIdx] = colIdx;
+  __syncthreads();
+  sharedMemExclusiveScan(colIdx, current_array, scanOut, scratch, POLAR_WIDTH);
+  
 
   //ErrorFunctionCu errFunc(source, sourceWidth, srcX, srcY, output, OUTPUT_WIDTH, mapX, mapY, PolarTransformation(MAX_RADIUS, RADIUS_FACTOR, ANGLE_FACTOR));
 
@@ -154,18 +163,23 @@ void imagequilt_cuda(int texture_width, int texture_height, unsigned char* sourc
   int seed = 15418;
   curandState *randStates;
   cudaMalloc((void**)&randStates, sizeof(curandState)*output_width*output_height);
-  int numBlocks = output_width*output_height/BLOCK_SIZE;
-
-  initRandom<<<numBlocks, BLOCK_SIZE>>>(seed, randStates);
-  kernelRandomOutput<<<numBlocks, BLOCK_SIZE>>>(randStates, 
+  dim3 tempblock(32,32);
+  int widthGrid = output_width*output_height/BLOCK_SIZE/BLOCK_SIZE;
+  dim3 randGrid(BLOCK_SIZE, widthGrid);
+  printf("Lolz %d\n",widthGrid);
+  
+  initRandom<<<randGrid, BLOCK_SIZE>>>(seed, randStates);
+  cudaDeviceSynchronize();
+  kernelRandomOutput<<<randGrid, BLOCK_SIZE>>>(randStates, 
                                                 output_cuda,
                                                 output_width,
                                                 texture_width*texture_height);
+  cudaDeviceSynchronize();
 
   dim3 blockDim(POLAR_WIDTH, 1);
   dim3 gridDim(WIDTH_TILES, HEIGHT_TILES, 1);
 
-  for (int iter = 0; iter < ITERATIONS; iter++)
+  /*for (int iter = 0; iter < ITERATIONS; iter++)
   {
     //choose random grid alignment
     const int offsetX = std::rand() % TILE_WIDTH;
@@ -173,7 +187,7 @@ void imagequilt_cuda(int texture_width, int texture_height, unsigned char* sourc
 
     kernelFindBoundaries<<<gridDim, blockDim>>>(randStates, source_cuda, texture_width, texture_height, output_cuda, offsetX, offsetY, min_paths, samplesX, samplesY);
     
-  }
+  }*/
 
   cudaMemcpy(output, output_cuda, output_size, cudaMemcpyDeviceToHost);
 
